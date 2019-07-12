a3ff487fb4c194f7df05a5955ae2d7b354bc8f18
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-07-12 21:14:56 +0530

    Buffered Channel example

---
+++ Concurrency_in_Go/Buffered Channels and worker pool/2-Buffered-Channel.go
@@ -0,0 +1,28 @@
+// Another example in which the values to the channel are written in a concurrent Goroutine and read from the main Goroutine. This example will help us better understand when writes to a buffered channel block.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func write(ch chan int) {
+	for i := 0; i < 5; i++ {
+		ch <- i
+		fmt.Println("successfully wrote", i, "to ch")
+	}
+	close(ch)
+}
+func main() {
+	ch := make(chan int, 2)
+	go write(ch)
+	time.Sleep(2 * time.Second)
+	for v := range ch {
+		fmt.Println("read value", v, "from ch")
+		time.Sleep(2 * time.Second)
+
+	}
+}
+
+
