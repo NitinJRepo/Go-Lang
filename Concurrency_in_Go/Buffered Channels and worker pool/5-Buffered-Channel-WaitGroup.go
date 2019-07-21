eee08d0d9a0272377d14a15d7361ea002ff6899b
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-07-21 18:30:10 +0530

    Added description

---
+++ Concurrency_in_Go/Buffered Channels and worker pool/5-Buffered-Channel-WaitGroup.go
@@ -0,0 +1,33 @@
+// A WaitGroup is used to wait for a collection of Goroutines to finish executing. The control is blocked until all Goroutines finish executing.
+// Lets say we have 3 concurrently executing Goroutines spawned from the main Goroutine. 
+// The main Goroutines needs to wait for the 3 other Goroutines to finish before terminating. This can be accomplished using WaitGroup.
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func process(i int, wg *sync.WaitGroup) {
+	fmt.Println("Started goroutine: ", i)
+	time.Sleep(2 *  time.Second)
+	fmt.Println("Ended goroutines: ", i)
+
+	wg.Done()
+}
+
+func main() {
+	no := 3
+	var wg sync.WaitGroup
+
+	for i := 0; i < no; i++ {
+		wg.Add(1)
+		go process(i, &wg)
+	}
+	wg.Wait()
+	fmt.Println("All goroutines finished executing")
+
+}
+
