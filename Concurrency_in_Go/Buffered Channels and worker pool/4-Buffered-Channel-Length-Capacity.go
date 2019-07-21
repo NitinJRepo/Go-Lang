b02dad2dd89223036f0c681e87fc5651f0a72196
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-07-21 17:40:39 +0530

    Added description

--- Concurrency_in_Go/Buffered Channels and worker pool/4-Buffered-Channel-Length-Capacity.go
+++ Concurrency_in_Go/Buffered Channels and worker pool/4-Buffered-Channel-Length-Capacity.go
@@ -1,3 +1,6 @@
+// The capacity of a buffered channel is the number of values that the channel can hold. This is the value we specify when creating the buffered channel using the make function.
+// The length of the buffered channel is the number of elements currently queued in it.
+
 package main
 
 import (
